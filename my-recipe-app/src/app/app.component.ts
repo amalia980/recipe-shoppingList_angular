c83329e522e20cfbbac56b29a08dd532bdd3a3a3
Amalia <74189129+chocolateprincess666@users.noreply.github.com>
2021-11-09 15:32:48 +0100

    added navigation through navbar, not best solution

--- my-recipe-app/src/app/app.component.ts
+++ my-recipe-app/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-recipe-app';
+
+  loadedFeature = 'recipe';
+
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }

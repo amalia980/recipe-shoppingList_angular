d5809fdd1c557eb46b92eba2452ea9563867d0fb
Amalia <74189129+chocolateprincess666@users.noreply.github.com>
2021-11-26 14:29:29 +0100

    fixing bug with the Resolver

--- my-recipe-app/src/app/recipes/recipes-resolver.service.ts
+++ my-recipe-app/src/app/recipes/recipes-resolver.service.ts
@@ -4,13 +4,19 @@ import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
 import { DataStorageService } from "../shared/data-storage.service";
 import { Recipe } from "./recipe.model";
+import { RecipeService } from "./recipe.service";
 
 @Injectable({providedIn: 'root'})
 export class RecipeResolverService implements Resolve<Recipe[]>//we have to define what type of data it will resolve
  {
-   constructor(private dataStorageService: DataStorageService) { }
+   constructor(private dataStorageService: DataStorageService, private recipesService: RecipeService) { }
 
    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-      return this.dataStorageService.fetchRecipes();
+     const recipes = this.recipesService.getRecipes();
+      if (recipes.length === 0) {//if this is true it means we have no recipes and we should fetch them
+        return this.dataStorageService.fetchRecipes();
+      } else {//or else we can just return these recipes instead
+        return recipes;
+      }
    }
 }

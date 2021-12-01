402f5cea7b1ccc52a2a31793e2ca6b7a31fcb52a
Amalia <74189129+chocolateprincess666@users.noreply.github.com>
2021-12-01 14:46:50 +0100

    added auto login and logout

--- my-recipe-app/src/app/auth.service.ts
+++ my-recipe-app/src/app/auth.service.ts
@@ -22,6 +22,8 @@ export class AuthService {
   //store the authenticated user as a BehaviourSubject, another type of subject
   user = new BehaviorSubject<User>(null);//has to have a starting value, null for example
   token: string = null;
+  //setTimeOut auto logout
+  private tokenExpirationTimer: any;
 
   constructor(private http: HttpClient, private router: Router) { }
 
@@ -60,21 +62,82 @@ export class AuthService {
   }
 
   //save user data for signup ang login
-  private handleAuth(email: string, userId: string,token: string, expiresIn: number) {
+  private handleAuth(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+    )
+    {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
-      const userData = new User(
+      const user = new User(
         email,
         userId,
         token,
         expirationDate
       );
-      this.user.next(userData);
+      this.user.next(user);
+
+      //when the auto logout should kick in, meaning when the token should be expired
+      this.autoLogout(expiresIn * 1000);
+
+
+      //store the token where it can survive where it can survive page reloads + browser restarts. you can do this by cookies or local storage which is an API exposed by the browser to store simple key value pairs. we will use local storage
+      localStorage.setItem('userData', JSON.stringify(user));//named key userData, store the user data in the key, convert data to a string
   }
+
+
+  //AUTO LOGIN
+  //to hinder us logging out when page is realoaded or browser restarted. as long as our token is still legit and not expired
+  autoLogin() {
+    const userData: {
+      email: string,
+      id: string,
+      _token: string,
+      _tokenExpirationDate: Date
+    } = JSON.parse(localStorage.getItem('userData'));//checking if there is an existing user snapshot stored. retrieve data from the local storage "userData". parse to a string again
+    if(!userData) {
+      return;//we cant log the user in, there is no stored data
+    }
+      //there was a snapshot we could retrieve
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)//convert to Data object again
+    );
+
+    if (loadedUser.token) {//if its true. token is a getter, comes from user.model
+      this.user.next(loadedUser);//we know we have a user with all the data loaded and the token is still valid. this is our logged in user. the AUTO LOGIN
+
+      //new Data "userData is the future date" that is to come -(minus) the new Date(get the current time), its like a countdown as soon as you log in
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+      this.autoLogout(expirationDuration);
+    }
+  }
+
 
   //logout option
   logout() {
     this.user.next(null);//set our user subject to null. make sure the app treats the user as unauthenticated
     this.router.navigate(['/auth']);//redirect to auth, to make sure no matter how we are logged out we we always redirect the user. we are not stuck on some random component
+
+    //clear the snapshot, the data, if the user logouts we have to clear everything
+    localStorage.removeItem('userData');
+
+    //check if we have an active timer
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  //AUTO LOGOUT
+  //set a timer for auto log out
+  autoLogout(expirationDuration: number) {
+    setTimeout(() => {
+     this.tokenExpirationTimer = this.logout();//calling the funtion for logging out
+    }, 3000);//auto logout in 3 sec
   }
 
   //this is the actual method to handle the error. but the logic will be executed above. on line 29 and 40
